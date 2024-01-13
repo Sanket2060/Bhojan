f4ebab4b43e31979a516a7b73ce7516b3c90f69b
Sanket2060 <114918951+Sanket2060@users.noreply.github.com>
2024-01-13 08:38:39 +0545

    Updated Button.tsx

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,24 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const Button = ({ buttonText }) => {
+const Button = ({ onClick, text, variant }) => {
+  const buttonStyles = {
+    base: "align-middle bg-[#261750] select-none font-bold  py-6 px-10 text-center  transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xl  bg-[261750] text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none rounded-full",
+    cancel: "align-middle select-none font-sans font-bold py-6 px-8 text-center uppercase transition-all bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full",
+    // Add more variants as needed
+  };
+
   return (
-    <button className="align-middle bg-[#261750] select-none font-bold  py-6 px-10 text-center  transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xl  bg-[261750] text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none rounded-full  ">
-      {buttonText}
+    <button onClick={onClick} className={buttonStyles[variant || "base"]}>
+      {text}
     </button>
   );
+};
+
+Button.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  text: PropTypes.string.isRequired, 
+  variant: PropTypes.string, 
 };
 
 export default Button;
