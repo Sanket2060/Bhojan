a56766b299f7f87cbb688621bab7ae64c74f62d7
Sulav2060 <99279765+Sulav2060@users.noreply.github.com>
2024-01-15 16:43:03 +0545

    Revert "Made navbar menu for mobile devce on landingpage"

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,22 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { RiMenu3Line } from "react-icons/ri";
 import Button from "./Button";
 
 const Navbar = () => {
-  const [menuOpen, setMenuOpen] = useState(false);
-  const handleMenuToggle = () => {
-    setMenuOpen(!menuOpen);
-  };
-  useEffect(() => {
-    if (menuOpen) {
-      document.body.style.overflow = "hidden";
-    } else {
-      document.body.style.overflow = "visible";
-    }
-    return () => {
-      document.body.style.overflow = "visible";
-    };
-  }, [menuOpen]);
   return (
     <div className=" ">
       <ul className="wrapper flex items-center justify-between">
@@ -44,48 +30,14 @@ const Navbar = () => {
         </div>
         <li>
           <a className=" " href="#">
-            <button
-              className="lg:hidden font:bold"
-              onClick={handleMenuToggle}
-              type="button"
-            >
-              <RiMenu3Line size={32} className="lg:hidden font:bold" />
-            </button>
+            <RiMenu3Line size={32} className="lg:hidden font:bold" />
+
             <button className="align-middle hidden lg:block select-none  py-3 px-10 text-center  transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xl  bg-[#261750] text-white  shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none rounded-full shadow-xl ring-1 ring-slate-900/5">
               Login
             </button>
           </a>
         </li>
       </ul>
-      {menuOpen && (
-        <div className="bg-white absolute w-screen h-[95vh] lg:hidden ">
-          <ul className=" flex-col gap-y-6 text-xl flex  mt-[30vh]  ">
-            <li className="self-center  ">
-              <a className="" href="#">
-                Placeholder
-              </a>
-            </li>
-            <li className="self-center">
-              <a className="" href="#">
-                Placeholder
-              </a>
-            </li>
-            <li className="self-center">
-              <a className="" href="#">
-                Placeholder
-              </a>
-            </li>
-            <li className="self-center mt-[10vh]">
-              <button className="align-middle  select-none  py-3 px-14 text-center  transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xl  bg-[#261750] text-white  shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none rounded-full shadow-xl ring-1 ring-slate-900/5">
-                Login
-              </button>
-            </li>
-          </ul>
-          <div className="overflow-hidden pt-10 pl-10">
-            <div className="h-[140vw] left-[50vw] bg-yellow-100 rounded-[70vw] w-[140vw]"></div>
-          </div>
-        </div>
-      )}
     </div>
   );
 };
