fdd3feaf48b0e6d18883f8f124925f87c2306804
sulav2060 <sulav2060@gmail.com>
2024-01-24 15:01:36 +0545

    Added ConfirmDistribution -2

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,6 +5,7 @@ import PropTypes from "prop-types";
 const Button = ({ onClick, text, variant }) => {
   const buttonStyles = {
     base: "align-middle bg-[#261750] select-none font-bold  py-6 px-10 text-center  transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xl  bg-[261750] text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none rounded-full",
+    complete: "align-middle select-none  py-6 px-10 text-center  transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xl  bg-[261750] text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] rounded-full",
     cancel: "align-middle select-none font-sans font-bold py-6 px-8 text-center uppercase transition-all bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full",
     // Add more variants as needed
   };
