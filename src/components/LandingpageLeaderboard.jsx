383bc8ad1585b0376bcbc2e0d38d9d8e1ff128b3
xdzc0 <p.friends705@gmail.com>
2024-02-16 21:18:49 +0545

    Fixed responsiveness of landingpageleaderboard

--- src/components/LandingpageLeaderboard.jsx
+++ src/components/LandingpageLeaderboard.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import User from "../assets/Catring.jpg";
 
 const LandingpageLeaderboard = ({ topContributors }) => {
   const [imageHeight, setImageHeight] = useState(null);
@@ -9,64 +8,62 @@ const LandingpageLeaderboard = ({ topContributors }) => {
   }, [imageHeight]);
 
   const handleImageLoad = (event) => {
-    const height = event.target.height;
+    const { height } = event.target;
     setImageHeight(height);
   };
 
   return (
     <div className="flex flex-col bg-gray-100 pt-10 dark:bg-[#1F1A24]">
-      {topContributors?.map((contributor, index) =>
-        index <= 2 ? (
-          <div
-            key={index}
-            className={`wrapper flex flex-col items-center lg:flex-row justify-between ${
-              index % 2 === 0 ? "lg:flex-row" : "lg:flex-row-reverse"
-            }`}
-            style={{
-              width: "100%",
-            }}
-          >
-            <div className="lg:w-[32rem]">
-              <div
-                className={`border-x-orange-300 ${
-                  index === 1 ? "border-s-8" : "border-e-8"
-                } rounded-full w-[100%] h-[100%] relative`}
-              >
-                <img
-                  className={`object-cover rounded-full ${
-                    imageHeight && imageHeight < 900 ? "bg-blue-500" : ""
-                  }`}
-                  src={contributor.avatar}
-                  alt="Contributor"
-                  onLoad={handleImageLoad}
-                  style={{
-                    height:
-                      imageHeight && imageHeight < 900
-                        ? `${imageHeight}px`
-                        : "100%",
-                  }}
-                />
-              </div>
-            </div>
+      {topContributors?.slice(0, 3).map((contributor, index) => (
+        <div
+          key={index}
+          className={`wrapper flex flex-col items-center lg:flex-row justify-between w-screen px-12 ${
+            index % 2 === 0 ? "lg:flex-row" : "lg:flex-row-reverse"
+          }`}
+        >
+          <div className="block relative ">
             <div
-              className={`lg:w-10/12 ${
-                index % 2 === 0 ? "lg:text-left" : "lg:text-right"
-              }`}
+              className={`border-x-orange-300  ${
+                index === 1 ? "border-s-8" : "border-e-8"
+              } rounded-full relative`}
             >
-              <div className="p-3">
-                <div className="text-2xl text-[#ff4c70] font-bold">
-                  {contributor.name}
-                </div>
-                <div className="text-xl text-grey-600 dark:text-gray-200">
-                  {contributor.address}
-                </div>
+              <img
+                className={`object-cover rounded-full h-[21rem] w-[21rem] 
+                {//"$"{
+                  //imageHeight && imageHeight < 900 ? "bg-blue-500" : ""
+                //}
+              }
+
+                `}
+                src={contributor.avatar}
+                alt="Contributor"
+                onLoad={handleImageLoad}
+                onError={() => console.error("Image loading failed")}
+                //style={{
+                //  height:
+                //    imageHeight && imageHeight < 900
+                //     ? `${imageHeight}px`
+                //     : "100%",
+                //}}
+              />
+            </div>
+          </div>
+          <div
+            className={`lg:w-9/12 ${
+              index % 2 === 0 ? "lg:text-left" : "lg:text-right"
+            }`}
+          >
+            <div className="p-3">
+              <div className="text-2xl text-[#ff4c70] font-bold">
+                {contributor.name}
+              </div>
+              <div className="text-xl text-grey-600 dark:text-gray-200">
+                {contributor.address}
               </div>
             </div>
           </div>
-        ) : (
-          <span key={index}></span>
-        )
-      )}
+        </div>
+      ))}
     </div>
   );
 };
